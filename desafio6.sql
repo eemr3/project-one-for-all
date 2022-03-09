e73db94103fdac7d6c2ffaa3d1025a234ec99f18
Emerson <eemr3@yahoo.com.br>
2022-03-09 18:07:15 -0300

    Tendo como base o valor dos planos e o plano que cada pessoa usuária cadastrada possui no banco, queremos algumas informações sobre o faturamento da empresa. Crie uma QUERY que deve exibir quatro dados:: requisito-6 desafio6.sql

---
+++ desafio6.sql
@@ -0,0 +1,5 @@
+SELECT  MIN(p.price) AS faturamento_minimo, MAX(p.price) AS faturamento_maximo,
+ROUND(AVG(p.price), 2) AS faturamento_medio, SUM(p.price) AS faturamento_total 
+FROM SpotifyClone.table_plan AS p
+INNER JOIN SpotifyClone.user AS us
+ON p.plan_id = us.plan_id;
